005838a47f5980dad62a5046b4edb07b8343ae98
ville <ville.koivukangas12@gmail.com>
2021-05-08 21:19:24 +0300

    Bot class

---
+++ bot/index.js
@@ -0,0 +1,45 @@
+/**
+ * Class that handles sendig messages to discord channel
+ */
+class BestDiscordBot {
+
+    clientChannels = [];
+    client = null;
+
+    /**
+     * Constructor
+     */
+    constructor() {
+    }
+
+    /**
+     * Sets channels
+     * @param {Discord.ChannelManager} channels 
+     */
+    setChannels(channels) {
+        this.clientChannels = channels;
+    }
+
+    /**
+     * Sends message to given channel
+     * 
+     * @param {String} channelName 
+     * @param {String} message 
+     */
+    sendMessageToChannel(channelName, message) {
+        const channel = this.getChannelByName(channelName);
+        channel.send(message);
+    }
+
+    /**
+     * Finds channel from list of channels
+     * 
+     * @param {String} name 
+     * @returns 
+     */
+    getChannelByName(name) {
+        return this.clientChannels.cache.find(clientChannel => clientChannel.name === name);
+    }
+}
+
+module.exports = new BestDiscordBot();
--- index.js
+++ index.js
@@ -1,37 +1,11 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const config = require('./config');
+const bot = require('./bot');
 
-client.login("ODQwNjM0MTYyMTIxMTQ2NDQ4.YJbDkg.RvrfCV4Ix3suHCfDoibFbvdpLo4");
+client.login(config.BOT_TOKEN);
 
 client.on("ready", () => {
-    const bot = new BestDiscordBot();
     bot.setChannels(client.channels);
-    bot.sendMessageToChannel('testi', 'Kakkaa :D');
+    bot.sendMessageToChannel(config.BOT_CHANNEL_NAME, 'Striimi käynnissä! https://www.twitch.tv/teamaquafi');
 });
- 
-client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
-  }
-});
-
-class BestDiscordBot {
-
-    clientChannels = [];
-
-    constructor() {
-
-    }
-
-    setChannels(channels) {
-        clientChannels = channels;
-    }
-
-    sendMessageToChannel(channelName, message) {
-        
-    }
-
-    getChannelByName(name) {
-        return this.clientChannels.find(clientChannel => clientChannel.name === name);
-    }
-}
