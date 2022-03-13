fc062bfab18ef808d51752224ba5738de0cf47c1
vsihvola <57141359+vsihvola@users.noreply.github.com>
2022-03-13 19:29:41 +0200

    Update config.js

--- build/config.js
+++ build/config.js
@@ -2,13 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.config = void 0;
 exports.config = {
-    "BOT_TOKEN": "ODQwNjM0MTYyMTIxMTQ2NDQ4.YJbDkg.RvrfCV4Ix3suHCfDoibFbvdpLo4",
-    "BOT_CHANNEL_NAME": "testi",
-    "TWITTER_API_KEY": "WODghhoXXJAaIpj1p5IhVMIND",
-    "TWITTER_API_SECRET_KEY": "fqXOnfeknA6w5a4HHRfG3KYByYrJVjsZUUX90Ha24YUC0FvO07",
-    "TWITTER_BEARER_TOKEN": "AAAAAAAAAAAAAAAAAAAAANhzPQEAAAAApr6lOhu8c09SQ3kssYkKyQqYNkY%3DMug1iTfOvSbObGl13vOAaqCegKAq1OESUlIcscO05EDDo6bY7u",
-    "TWITTER_WEBHOOK_ENV": "DEV",
-    "TWITCH_CLIENT_ID": "83pc8ghvtwnoevo7rm8l3eqo98i89k",
-    "TWITCH_SECRET": "dqjoyuaye4z53abp2825yuyyt7lwjc",
-    "TWITCH_USER_ID": "684948045"
+
 };
