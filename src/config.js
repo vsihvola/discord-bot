83266b58295d7cfd7aae749ee91b50f7c4266dd9
vsihvola <57141359+vsihvola@users.noreply.github.com>
2022-03-13 19:28:58 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,11 +1,1 @@
-export const config = {
-  "BOT_TOKEN": "ODQwNjM0MTYyMTIxMTQ2NDQ4.YJbDkg.RvrfCV4Ix3suHCfDoibFbvdpLo4",
-  "BOT_CHANNEL_NAME" : "testi",
-  "TWITTER_API_KEY": "WODghhoXXJAaIpj1p5IhVMIND",
-  "TWITTER_API_SECRET_KEY": "fqXOnfeknA6w5a4HHRfG3KYByYrJVjsZUUX90Ha24YUC0FvO07",
-  "TWITTER_BEARER_TOKEN": "AAAAAAAAAAAAAAAAAAAAANhzPQEAAAAApr6lOhu8c09SQ3kssYkKyQqYNkY%3DMug1iTfOvSbObGl13vOAaqCegKAq1OESUlIcscO05EDDo6bY7u",
-  "TWITTER_WEBHOOK_ENV" : "DEV",
-  "TWITCH_CLIENT_ID": "83pc8ghvtwnoevo7rm8l3eqo98i89k",//"hb1vqw47cg5td2kpxyx7db05axx61r",
-  "TWITCH_SECRET" : "dqjoyuaye4z53abp2825yuyyt7lwjc",//"ye8qc0qud27conlrn6gg8o4yrrbmgy",
-  "TWITCH_USER_ID": "684948045"
-};
+
